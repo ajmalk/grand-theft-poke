bc9819d006bcaf675aa3c82e1e92d01f528f668b
Ajmal Kunnummal <ajmal@gatech.edu>
2012-11-06 02:15:27 -0500

    Merge stuff

--- GrandTheftPoke/grand-theft-poke/src/edu/gatech/CS2340/GrandTheftPoke/screens/Name.java
+++ GrandTheftPoke/grand-theft-poke/src/edu/gatech/CS2340/GrandTheftPoke/screens/Name.java
@@ -13,34 +13,38 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.Button.ButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField.TextFieldListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener.ChangeEvent;
 
 import edu.gatech.CS2340.GrandTheftPoke.GTPoke;
+import edu.gatech.CS2340.GrandTheftPoke.backend.Main;
 
 public class Name extends AbstractScreen {
 	private Texture background;
-	
+
 	private Button ConfirmButton;
-	
+
 	TextField name;
-	
-	private	Table table;
-	
-	public Name(GTPoke game){
+	private String playerName = "";
+
+	private Table table;
+
+	public Name(GTPoke game) {
 		super(game);
 	}
+
 	@Override
-	public void show(){
+	public void show() {
 		super.show();
-		
+
 		table = new Table(getSkin());
 		table.setFillParent(true);
-		
+
 		background = new Texture("images//main-menu.png");
-		
+
 		Texture ButtonSprite = new Texture("images//button-sprite.png");
 		stage.clear();
 
@@ -48,67 +52,86 @@ public class Name extends AbstractScreen {
 		backgroundImage.setFillParent(true);
 		backgroundImage.getColor().a = 0f;
 		backgroundImage.addAction(fadeIn(0.75f));
-		stage.addActor( backgroundImage );
-		
+		stage.addActor(backgroundImage);
+
 		name = new TextField("", getSkin());
-		name.addListener(new ChangeListener() {
+		name.setTextFieldListener(new TextFieldListener() {
 			@Override
-			public void changed(ChangeEvent event, Actor actor) {
-				ConfirmButton.setDisabled(false);
-				ConfirmButton.setTouchable(Touchable.enabled);
+			public void keyTyped(TextField name, char key) {
+				System.out.print(key);
+				if(key == '\b' && playerName.length() != 0) {
+					playerName = playerName.substring(0, playerName.length()- 1);
+				}
+				else if(key != '\b'){
+					playerName += key;
+				}
+				
+				if (playerName.length() != 0) {
+					ConfirmButton.setDisabled(false);
+					ConfirmButton.setTouchable(Touchable.enabled);
+				}
+				else {
+					ConfirmButton.setDisabled(true);
+					ConfirmButton.setTouchable(Touchable.disabled);
+				}
 			}
 		});
-		
+
 		ButtonStyle style = new ButtonStyle();
-		style.up = new TextureRegionDrawable(new TextureRegion(ButtonSprite, 0, 0, 320, 70));
-		style.down = new TextureRegionDrawable(new TextureRegion(ButtonSprite, 0, 69, 320, 70));
-		style.disabled = new TextureRegionDrawable(new TextureRegion(ButtonSprite, 0, 69, 320, 70)); 
-		
+		style.up = new TextureRegionDrawable(new TextureRegion(ButtonSprite, 0,
+				0, 320, 70));
+		style.down = new TextureRegionDrawable(new TextureRegion(ButtonSprite,
+				0, 69, 320, 70));
+		style.disabled = new TextureRegionDrawable(new TextureRegion(
+				ButtonSprite, 0, 69, 320, 70));
+
 		ConfirmButton = new Button(style);
-		
+		ConfirmButton.setDisabled(true);
+		ConfirmButton.setTouchable(Touchable.disabled);
+
 		ConfirmButton.setSkin(getSkin());
 		ConfirmButton.add("Confirm");
 		ConfirmButton.setPosition(700, 10);
-		//ConfirmButton.setDisabled(true);
-		//ConfirmButton.setTouchable(Touchable.disabled);
-		
+		// ConfirmButton.setDisabled(true);
+		// ConfirmButton.setTouchable(Touchable.disabled);
+
 		ConfirmButton.setStyle(style);
-		
+
 		ConfirmButton.addListener(new ClickListener() {
-			public void clicked (InputEvent event, float x, float y) {
+			public void clicked(InputEvent event, float x, float y) {
+				game.setPlayerName(playerName);
 				game.setScreen(game.getSkillPointsScreen());
 			}
 		});
 
 	}
-	
+
 	@Override
-	public void render(float delta){
+	public void render(float delta) {
 		super.render(delta);
 
-		//remVal.setText(remainder.toString());
-		
+		// remVal.setText(remainder.toString());
+
 		table.clear();
-		
+
 		table.add("Name:").width(100);
 		table.add(name);
 		table.row();
-		
+
 		stage.addActor(table);
 		stage.addActor(ConfirmButton);
-		//table.debug();
-		//table.drawDebug(stage);
-		
-		
+		// table.debug();
+		// table.drawDebug(stage);
+
 	}
 
 	@Override
-	public void resize(int width, int height){
+	public void resize(int width, int height) {
 		super.resize(width, height);
 	}
 
 	@Override
-	public void dispose(){
+	public void dispose() {
 		super.dispose();
 		background.dispose();
 	}
