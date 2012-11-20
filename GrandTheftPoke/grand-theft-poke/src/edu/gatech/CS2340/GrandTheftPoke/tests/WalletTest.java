c8f845018713f0623af0ef848650ec5ec31071db
jillian cagliostro <lilcagz88@gmail.com>
2012-11-20 16:59:05 -0500

    javadoc for junit test

--- GrandTheftPoke/grand-theft-poke/src/edu/gatech/CS2340/GrandTheftPoke/tests/WalletTest.java
+++ GrandTheftPoke/grand-theft-poke/src/edu/gatech/CS2340/GrandTheftPoke/tests/WalletTest.java
@@ -9,9 +9,14 @@ import edu.gatech.CS2340.GrandTheftPoke.backend.MarketPlaceItem;
 import edu.gatech.CS2340.GrandTheftPoke.backend.Wallet;
 import edu.gatech.CS2340.GrandTheftPoke.backend.Items.Item;
 import edu.gatech.CS2340.GrandTheftPoke.backend.Items.GlobalItemReference;
-
+/**
+ * Tests the Wallet class to see if the amounts are updated correctly
+ * @author Jillian Cagliostro 
+ * @version 1.2
+ */
 
 public class WalletTest extends junit.framework.TestCase {
+
 	private final float AMOUNT = 100;
 	private Wallet wallet= new Wallet(AMOUNT);;
 	private GlobalItemReference items;
@@ -19,19 +24,23 @@ public class WalletTest extends junit.framework.TestCase {
 	private float price, newAmount;
 
 	/**
-	 * toString
+	 * To string
 	 * @return the name of the class
 	 */
 	@Override
 	public String toString() {
 		return "WalletTest";
 	}
-	
-	
+	/**
+	 * Checks to make sure the wallets getMoney method works
+	 */
 	@Test
 	public void testGetMoney(){
 		assertEquals(AMOUNT, wallet.getMoney());
 	}
+	/**
+	 * Creates a mini market place for testing purposes
+	 */
 	public void setup(){
 		items = new GlobalItemReference();
 		
@@ -40,18 +49,23 @@ public class WalletTest extends junit.framework.TestCase {
 		myStock.put(items.getStrengthRoid(), new MarketPlaceItem(1500f, 15f));
 		myStock.put(items.getTradeRoid(), new MarketPlaceItem(5000f, 5f));
 		
-				myMarket = new MarketPlace(myStock);
-		
+		myMarket = new MarketPlace(myStock);
 	}
+	/**
+	 * Checks that when you update the price for a sale, that it updates to the correct price
+	 */
 	@Test
 	public void testUpdateMoney(){
 		setup();
-		MarketPlaceItem testitem = (MarketPlaceItem) myMarket.getStock().get(items.getStrengthRoid());
-		price = testitem.getPrice();
+		
+		price = ((MarketPlaceItem) myMarket.getStock().get(items.getStrengthRoid())).getPrice();
 		wallet.updateMoney(-price);
 		newAmount = AMOUNT - price;
 		assertEquals(wallet.getMoney(),newAmount);
 	}
+	/**
+	 * Checks that the CheckAmount returns the correct boolean value
+	 */
 	@Test
 	public void testCheckAmount(){
 		setup();
