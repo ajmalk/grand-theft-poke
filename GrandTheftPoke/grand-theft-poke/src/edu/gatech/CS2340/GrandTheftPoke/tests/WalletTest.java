c0bd03f778d22f5d1aac84a4067ed30df59cb3fc
jillian cagliostro <lilcagz88@gmail.com>
2012-11-20 16:53:52 -0500

    fixed junit test

--- GrandTheftPoke/grand-theft-poke/src/edu/gatech/CS2340/GrandTheftPoke/tests/WalletTest.java
+++ GrandTheftPoke/grand-theft-poke/src/edu/gatech/CS2340/GrandTheftPoke/tests/WalletTest.java
@@ -1,15 +1,23 @@
 package edu.gatech.CS2340.GrandTheftPoke.tests;
 
-//import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.*;
-
-//import org.junit.Before;
+import java.util.HashMap;
 import org.junit.Test;
 
+import edu.gatech.CS2340.GrandTheftPoke.backend.MarketPlace;
+import edu.gatech.CS2340.GrandTheftPoke.backend.MarketPlaceItem;
 import edu.gatech.CS2340.GrandTheftPoke.backend.Wallet;
+import edu.gatech.CS2340.GrandTheftPoke.backend.Items.Item;
+import edu.gatech.CS2340.GrandTheftPoke.backend.Items.GlobalItemReference;
+
 
 public class WalletTest extends junit.framework.TestCase {
-	private final float AMOUNT = 10;
+	private final float AMOUNT = 100;
+	private Wallet wallet= new Wallet(AMOUNT);;
+	private GlobalItemReference items;
+	private MarketPlace myMarket;
+	private float price, newAmount;
+
 	/**
 	 * toString
 	 * @return the name of the class
@@ -18,25 +26,40 @@ public class WalletTest extends junit.framework.TestCase {
 	public String toString() {
 		return "WalletTest";
 	}
+	
+	
 	@Test
-	public void testWallet(){
-		
-		Wallet wallet = new Wallet(AMOUNT);
-		float money;
-		money = wallet.getMoney();
-		assertEquals(money,AMOUNT);
+	public void testGetMoney(){
+		assertEquals(AMOUNT, wallet.getMoney());
 	}
+	public void setup(){
+		items = new GlobalItemReference();
+		
+		HashMap<Item, MarketPlaceItem> myStock = new HashMap<Item, MarketPlaceItem>(); 
+		//create some stock
+		myStock.put(items.getStrengthRoid(), new MarketPlaceItem(1500f, 15f));
+		myStock.put(items.getTradeRoid(), new MarketPlaceItem(5000f, 5f));
+		
+				myMarket = new MarketPlace(myStock);
+		
+	}
+	@Test
 	public void testUpdateMoney(){
-		Wallet wallet = new Wallet((float) 0);
-		wallet.updateMoney(AMOUNT);
-		assertEquals(wallet.getMoney(),AMOUNT);
+		setup();
+		MarketPlaceItem testitem = (MarketPlaceItem) myMarket.getStock().get(items.getStrengthRoid());
+		price = testitem.getPrice();
+		wallet.updateMoney(-price);
+		newAmount = AMOUNT - price;
+		assertEquals(wallet.getMoney(),newAmount);
 	}
+	@Test
 	public void testCheckAmount(){
-		Wallet wallet1 = new Wallet(AMOUNT);
-		Wallet wallet2 = new Wallet ((float)0);
+		setup();
 		float money;
-		money = wallet2.getMoney();
-		assertTrue(wallet1.checkAmount(money));
-		
+		boolean test;
+		price = ((MarketPlaceItem) myMarket.getStock().get(items.getTradeRoid())).getPrice();
+		money = wallet.getMoney();
+		test = wallet.checkAmount(price);
+		assertEquals((money>=price),test);
 	}
 }
